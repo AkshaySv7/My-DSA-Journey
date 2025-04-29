6d7c7d273e343769f491e5711b55806a1f8ddc2c
AkshaySv7 <akshaysv977@gmail.com>
2025-04-29 22:35:14 +0530

    Create tuple.py
    create a tuple and access it elemets

---
+++ Python/Lists/tuple.py
@@ -0,0 +1,9 @@
+name = input("Enter student name: ")
+age = int(input("Enter age: "))
+grade = input("Enter grade: ")
+
+student = (name, age, grade)
+
+print("Name:", student[0])
+print("Age:", student[1])
+print("Grade:", student[2])
